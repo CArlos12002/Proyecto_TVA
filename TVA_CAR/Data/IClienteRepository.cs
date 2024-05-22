4b9e7cea5381436a5aa4fb6515810abbac409aa7
CArlos12002 <118774337+CArlos12002@users.noreply.github.com>
2024-05-21 18:06:56 -0600

    Add files via upload

---
+++ TVA_CAR/Data/IClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TVA_CAR.Models;
+
+namespace TVA_CAR.Data
+{
+    public interface IClienteRepository
+    {
+        IEnumerable<Cliente> GetClientes(string nombre, string clave, string estatus);
+    }
+}
---
+++ TVA_CAR/Data/IProductoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TVA_CAR.Models;
+
+namespace TVA_CAR.Data
+{
+    public interface IProductoRepository
+    {
+        IEnumerable<Producto> GetProductos();
+    }
+}
